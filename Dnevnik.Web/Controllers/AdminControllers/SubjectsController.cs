0169689fedf22d5ab6a7e35ae55d92d3876cac64
snaksa <snaksa2@live.com>
2016-02-05 23:16:25 +0200

    Add subject

---
+++ Dnevnik.Web/Controllers/AdminControllers/SubjectsController.cs
@@ -0,0 +1,67 @@
+﻿using Dnevnik.Data.Repositories;
+using Dnevnik.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dnevnik.Web.Controllers.AdminControllers
+{
+    public class SubjectsController : AdminController
+    {
+        // GET: Subjects
+        public ActionResult Show()
+        {
+            var subjects = SubjectsRepository.GetAllSubjects();
+
+            SubjectsViewModel vm = new SubjectsViewModel()
+            {
+                Subjects = subjects
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddSubject(SubjectsViewModel vm)
+        {
+            string title = vm.Title;
+            try
+            {
+                if (title != null && title != string.Empty)
+                {
+                    SubjectsRepository.AddSubject(title);
+                    TempData["subjectAdded"] = "1";
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["subjectAdded"] = "0";
+            }
+
+            return RedirectToAction("Show");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateSubjects(SubjectsViewModel vm)
+        {
+            try
+            {
+                if (vm.Subjects != null)
+                {
+                    SubjectsRepository.UpdateSubjects(vm.Subjects);
+                    TempData["success"] = "1";
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["success"] = "0";
+            }
+
+            return RedirectToAction("Show");
+        }
+    }
+}
---
+++ Dnevnik.Web/ViewModels/SubjectsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Dnevnik.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dnevnik.Web.ViewModels
+{
+    public class SubjectsViewModel
+    {
+        public List<Subject> Subjects { get; set; }
+
+        public string Title { get; set; }
+    }
+}
